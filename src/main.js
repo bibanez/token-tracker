9af9da6ad429e6238614b4e31f8dccf880238dcb
Bernat Ibáñez <personal@bibanez.xyz>
2023-07-31 23:32:31 +0200

    New tokens detector

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ require('dotenv').config();
 const fs = require('fs').promises; // Use fs.promises for async/await
 const { Alchemy, Network } = require("alchemy-sdk");
 
+const filePath = process.env.ACCOUNTS_FILE;
+
+if (!filePath) {
+    console.error('Please set the ACCOUNTS_FILE environment variable.');
+    process.exit(1);
+}
+
 // Configures the Alchemy SDK
 const config = {
     apiKey: process.env.ALCHEMY_API_KEY, // Replace with your API key
@@ -12,37 +19,64 @@ const config = {
 // Creates an Alchemy object instance with the config to use for making requests
 const alchemy = new Alchemy(config);
 
-const filePath = process.env.ACCOUNTS_FILE;
+let tokens = new Set();
+let newTokens = new Set();
 
-if (!filePath) {
-    console.error('Please set the ACCOUNTS_FILE environment variable.');
-    process.exit(1);
-}
-
-const getTokenBalances = async (address) => {
+const generateTokenSet = async (address, tokenSet) => {
     const balances = await alchemy.core.getTokenBalances(address);
-    console.log(`The balances of ${address} address are:`, balances);
+
+    for (const item of balances.tokenBalances) {
+        tokenSet.add(item.contractAddress);
+    }
 }
+
+let occupied = false;
 
 const main = async () => {
+    occupied = true;
+
     try {
         // Read the JSON file using fs.promises.readFile and await it
         const data = await fs.readFile(filePath, 'utf8');
         const accounts = JSON.parse(data);
 
+        tokens.clear();
+        tokens = newTokens;
+        newTokens = new Set();
+
         for (const key in accounts) {
             try {
-                await getTokenBalances(accounts[key]["ethereum"]);
+                await generateTokenSet(accounts[key]["ethereum"], newTokens);
             } catch (error) {
                 console.log(error);
+                process.exit(1);
+            }
+        }
+
+        for (const token of newTokens) {
+            if (!tokens.has(token)) {
+                try {
+                    const metadata = await alchemy.core.getTokenMetadata(token);
+                    console.log("New token:", metadata.name);
+                } catch (error) {
+                    console.log(error);
+                    process.exit(1);
+                }
             }
         }
     } catch (err) {
         console.error('Error reading or parsing the file:', err);
     }
+
+    occupied = false;
 }
 
 alchemy.ws.on("block", async (blockNumber) => {
-    console.log("Latest block:", blockNumber);
-    await main();
+    if (!occupied) {
+        console.log("Latest block:", blockNumber);
+        await main();
+    }
+    else {
+        console.log("(new block received in the background)");
+    }
 });
